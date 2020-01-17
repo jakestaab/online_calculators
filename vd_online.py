bcac1b613c4adafcdb3a9d646ab90a800288a9ae
jakestaab <53784061+jakestaab@users.noreply.github.com>
2020-01-16 19:53:09 -0600

    Update vd_online.py

--- vd_online.py
+++ vd_online.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, RadioField, SelectField, IntegerField
-import requests
+from nec_lib import NECTables as NEC
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '987kjhgh(*Lz4d09wfh'
+
 
 class VD_Form(FlaskForm):
     material = RadioField('Material', choices=[(12.9, "CU"), (21.2, "AL")])
@@ -17,25 +18,32 @@ class VD_Form(FlaskForm):
 
 
 def get_voltage_drop(material, phase, size, length, current, voltage):
-    mtrl = request.form.get(material)
-    phs = request.form.get(phase)
-    sz = request.form.get(size)
-    lngth = request.form.get(length)
-    crnt = request.form.get(current)
-    vltg = request.form.get(voltage)
+    form = VD_Form()
+    if form.is_submitted():
+        mtrl = float(request.form.get(material))
+        phs = float(request.form.get(phase))
+        sz = str(request.form.get(size))
+        lngth = int(request.form.get(length))
+        crnt = int(request.form.get(current))
+        vltg = int(request.form.get(voltage))
 
-    numerator = (phs * mtrl * crnt * lngth)
+        numerator = (phs * mtrl * lngth * crnt)
+        denominator = NEC.awg_to_circmils[sz]
+        resulting_voltage = vltg - (numerator / denominator)
+        percentage = resulting_voltage / vltg
+        final = round(((1 - percentage) * 100), 3)
+
+        return final
+    else:
+        return 0
 
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/index", methods=['GET', 'POST'])
 def voltage_drop():
     form = VD_Form()
-    mate = get_voltage_drop('material')
-    return render_template('index.html', form=form, mate=mate)
-
-
-
+    vd_percent = get_voltage_drop('material', 'phase', 'size', 'length', 'current', 'voltage')
+    return render_template('index.html', form=form, vd=vd_percent)
 
 
 
